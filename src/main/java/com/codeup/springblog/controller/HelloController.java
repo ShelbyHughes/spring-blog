d6437e49f55350f9e9b33027f66739c12e8421e9
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-03-13 11:57:22 -0500

    finished first exercise

---
+++ src/main/java/com/codeup/springblog/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.codeup.springblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        System.out.println("Inside hello()");
+        return "Hello from Spring!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        System.out.println("Inside sayHello()");
+        return "Hello " + name + "!";
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+}
---
+++ src/main/java/com/codeup/springblog/controller/MathController.java
@@ -0,0 +1,32 @@
+package com.codeup.springblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{number}/and/{numbertwo}")
+    @ResponseBody
+    public String add(@PathVariable int number, @PathVariable int numbertwo) {
+        return number + " + " + numbertwo + " = " + (number+numbertwo);
+    }
+
+    @GetMapping("/subtract/{number}/from/{numbertwo}")
+    @ResponseBody
+    public String subtract(@PathVariable int number, @PathVariable int numbertwo){
+        return numbertwo + " - " +  number + " = " + (numbertwo-number);
+    }
+
+    @GetMapping("/multiply/{number}/and/{numbertwo}")
+    @ResponseBody
+    public String multiply(@PathVariable int number, @PathVariable int numbertwo){
+        return number + " x " +  numbertwo + " = " + (number*numbertwo);
+    }
+
+    @GetMapping("/divide/{number}/by/{numbertwo}")
+    @ResponseBody
+    public String divide(@PathVariable int number, @PathVariable int numbertwo){
+        return number + " % " +  numbertwo + " = " + (number/numbertwo);
+    }
+}
