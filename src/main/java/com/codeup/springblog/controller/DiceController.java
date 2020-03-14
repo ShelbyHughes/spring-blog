d99f8775872ecca80a1ebeacb35c8dea7cb5a4de
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-03-14 17:50:49 -0500

    roll dice series (bonus); add header, navbar, footer to roll dice page

--- src/main/java/com/codeup/springblog/controller/DiceController.java
+++ src/main/java/com/codeup/springblog/controller/DiceController.java
@@ -11,15 +11,25 @@ public class DiceController {
 
     @GetMapping("/roll-dice/{n}")
     public String rollDice(@PathVariable int n, Model model) {
-        Random dice = new Random();
-        int roll = dice.nextInt(5) + 1;
-        model.addAttribute("rolled", roll);
+        Random rand = new Random();
+//        int roll = rand.nextInt(5) + 1;
+
+        int[] dice = new int[6];
+        for(int i = 0; i < dice.length; i++){
+            dice[i] = rand.nextInt(6)+1; //(0 thru 5) + 1
+        }
+
+        model.addAttribute("dice", dice);
         model.addAttribute("guess", n);
-        if(roll == n){
-            model.addAttribute("resultMessage","You guessed correctly!");
+        int correctCount = 0;
+        for(int die : dice) {
+            if(n == die) correctCount++;
+        }
+        if(correctCount > 0){
+            model.addAttribute("resultMessage",String.format("You guessed %d out of %d correctly!", correctCount, dice.length));
 //            return "You guessed correctly!";
         }else{
-            model.addAttribute("resultMessage", "Better luck next time!");
+            model.addAttribute("resultMessage", String.format("You guessed 0 out of %d correctly. Better luck next time!", dice.length));
 //            return "Better luck next time!";
         }
         return "roll-dice-n";
@@ -27,6 +37,7 @@ public class DiceController {
 
     @GetMapping("/roll-dice")
     public String showRollDice(Model model) {
+        model.addAttribute("title","Roll Dice");
         model.addAttribute("welcome", "Select a number to guess");
         return "roll-dice";
     }
