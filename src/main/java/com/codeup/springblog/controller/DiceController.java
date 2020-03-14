6aa351c51705ce9eec84586d568531723656794d
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-03-14 17:13:02 -0500

    dice roll working (no bonus)

--- src/main/java/com/codeup/springblog/controller/DiceController.java
+++ src/main/java/com/codeup/springblog/controller/DiceController.java
@@ -1,4 +1,33 @@
 package com.codeup.springblog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@Controller
 public class DiceController {
+
+    @GetMapping("/roll-dice/{n}")
+    public String rollDice(@PathVariable int n, Model model) {
+        Random dice = new Random();
+        int roll = dice.nextInt(5) + 1;
+        model.addAttribute("rolled", roll);
+        model.addAttribute("guess", n);
+        if(roll == n){
+            model.addAttribute("resultMessage","You guessed correctly!");
+//            return "You guessed correctly!";
+        }else{
+            model.addAttribute("resultMessage", "Better luck next time!");
+//            return "Better luck next time!";
+        }
+        return "roll-dice-n";
+    }
+
+    @GetMapping("/roll-dice")
+    public String showRollDice(Model model) {
+        model.addAttribute("welcome", "Select a number to guess");
+        return "roll-dice";
+    }
 }
--- src/main/java/com/codeup/springblog/controller/HelloController.java
+++ src/main/java/com/codeup/springblog/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.codeup.springblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 class HelloController {
@@ -13,11 +17,17 @@ class HelloController {
         return "Hello from Spring!";
     }
 
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String sayHello(@PathVariable String name) {
+//        System.out.println("Inside sayHello()");
+//        return "Hello " + name + "!";
+//    }
+
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String sayHello(@PathVariable String name) {
-        System.out.println("Inside sayHello()");
-        return "Hello " + name + "!";
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
@@ -25,4 +35,35 @@ class HelloController {
     public String addOne(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
     }
+
+    @GetMapping("/fruit")
+    public String fruit(Model model){
+        List<String> fruitBasket = new ArrayList<>();
+        fruitBasket.add("apple");
+        fruitBasket.add("kiwi");
+        fruitBasket.add("pear");
+        fruitBasket.add("mango");
+        fruitBasket.add("watermelon");
+        fruitBasket.add("grapes");
+        fruitBasket.add("starfruit");
+        fruitBasket.add("papaya");
+        fruitBasket.add("japanese pear");
+        fruitBasket.add("pineapple");
+        fruitBasket.add("lemon");
+        fruitBasket.add("durian");
+        model.addAttribute("fruitBasket", fruitBasket);
+        return "fruit";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
 }
